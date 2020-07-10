59762134ef8357188153b18bdc73c388fc80a90c
Nazim Uddin <umnazim@gmail.com>
2020-07-09 22:19:27 -0700

    Fibonacci number

--- Factorial.cpp
+++ Factorial.cpp
@@ -1,19 +1,15 @@
 
 #include <iostream>
-
 using namespace std;
 
 int factorial(int n)
 {
     // base case
-
     if (n == 1)
         return 1;
-
     // reduce the problem and store states on the stack
     return n * factorial(n - 1);
 }
-
 
 // accumulator to track the result so far
 
@@ -22,16 +18,13 @@ int factorialAccumulator(int n, int accumulator)
     // define base case
     if (n == 1)
         return accumulator;
-
     // tail recursion
     return factorialAccumulator(n - 1, n * accumulator);
 }
 
 int main()
 {
-
     //accumulator value is 1 at the beginning
-    cout << factorialAccumulator(5, 1) << endl;  // 120
-
+    cout << factorialAccumulator(5, 1) << endl; // 120
     return 0;
 }
---
+++ Fibonacci.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n)
+{
+    //define base case
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    //formula for Fibonacci numbers
+    int fib1 = fibonacci(n - 1);
+    int fib2 = fibonacci(n - 2);
+    return fib1 + fib2;
+}
+
+int main()
+{
+    cout << fibonacci(10) << endl;  // 55, 10th Fibonacci number
+    return 0;
+}
